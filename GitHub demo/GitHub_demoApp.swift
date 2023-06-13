b6ba3571f57d7e308d1254beb505409db8807f14
Cameron Oberlies <cameron.oberlies@outlook.com>
2023-06-13 12:35:47 -0500

    Initial Commit

---
+++ GitHub demo/GitHub_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_demoApp.swift
+//  GitHub demo
+//
+//  Created by Cameron Oberlies on 6/13/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
